dc3cd94f06a053377aa74bfc4a98d04732ce590a
Sravya Kolli <sravyakolli305@gmail.com>
2021-12-23 14:25:03 +0530

    Using standard date instead of moment

--- src/components/WorldClock.js
+++ src/components/WorldClock.js
@@ -1,18 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import Moment from 'react-moment';
 import 'moment-timezone';
+import moment from 'moment';
 
 function WorldClock({ time, onClick }) {
-    const day = Date.UTC();
-    console.log(day);
+    const timestamp = moment().tz(time.zone);
+    const [hours, setHours] = useState(timestamp.hours())
+    const [minutes, setMinutes] = useState(timestamp.minutes())
+    const [seconds, setSeconds] = useState(timestamp.seconds())
+
+    setInterval(() => {
+        const newTime = timestamp.add(1, 's');
+        setHours(newTime.hours());
+        setMinutes(newTime.minutes());
+        setSeconds(newTime.seconds());
+    }, 1000);
+
     return (
-        <div className='clock-container' onClick={() => {onClick(time)}}>
-            <Moment format="HH:mm:ss" interval={1000} tz={time.zone} />
-            <div class="circle">
-                <div class="hours" ></div>
-                <div class="minutes"></div>
-                <div class="seconds"></div>
+        <div className='clock-container' onClick={() => { onClick(time) }}>
+            {/* <Moment format="HH:mm:ss" interval={1000} tz={time.zone} /> */}
+            <span>{`${hours} : ${minutes} : ${seconds}`}</span>
+            <div className="circle">
+                <div className="hours" ></div>
+                <div className="minutes"></div>
+                <div className="seconds"></div>
             </div>
             <label>{time.zone}</label>
         </div>
